6f4fbc93ce5bf5382eee102a9f16cfb54ae937d7
tshimegabelmont <tshimegabelmont@gmail.com>
2023-12-23 14:10:14 +0200

    FEAT: Translating the context vertically based on the taxi's position to simulate the taxi moving up the road.

--- juta.js
+++ juta.js
@@ -1,6 +1,6 @@
 // Defining Juta Street in Jozi CBD
 class Juta {
-  constructor(x, width, laneCount = 4) {
+  constructor(x, width, laneCount = 3) {
     this.x = x; // The center of the road
     this.width = width; // The width of Juta Street
     this.laneCount = laneCount; // How many lanes Juta Street has
@@ -11,12 +11,28 @@ class Juta {
     const infinity = 10000000; // Length of Juta Street
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
   // Find the center of a lane
   getLaneCenter(laneIndex) {
-    // left -> right '0 | 1 | 2 | 3'
     const laneWidth = this.width / this.laneCount;
-    return this.left + laneWidth / 2 + laneIndex * laneWidth;
+
+    // Ensure that laneIndex is within the valid range
+    const clampedLaneIndex = Math.max(
+      0,
+      Math.min(laneIndex, this.laneCount - 1)
+    );
+
+    return this.left + laneWidth / 2 + clampedLaneIndex * laneWidth;
   }
 
   // Drawing the white boundaries of Juta Street
@@ -25,20 +41,24 @@ class Juta {
     ctx.strokeStyle = "white";
 
     // Drawing the white lanes of Juta Street
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       //   Linearly interpolate (LERP) to estimate the values that lie between known values. In this case between 'i' and the laneCount
       const x = lerp(this.left, this.right, i / this.laneCount);
 
-      if (i > 0 && i < this.laneCount) {
-        ctx.setLineDash([20, 20]);
-      } else {
-        ctx.setLineDash([]);
-      }
+      ctx.setLineDash([20, 20]);
 
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ jozi.width = 200;
 // Creating the JHB context for the taxi
 const ctx = jozi.getContext("2d");
 const juta = new Juta(jozi.width / 2, jozi.width * 0.9);
-const taxi = new Taxi(juta.getLaneCenter(2), 100, 30, 50);
+const taxi = new Taxi(juta.getLaneCenter(1), 100, 30, 50);
 taxi.draw(ctx);
 
 // Animating the movement of the Taxi
@@ -13,7 +13,13 @@ animate();
 function animate() {
   taxi.update();
   jozi.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -taxi.y + jozi.height * 0.7);
+
   juta.draw(ctx);
   taxi.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
